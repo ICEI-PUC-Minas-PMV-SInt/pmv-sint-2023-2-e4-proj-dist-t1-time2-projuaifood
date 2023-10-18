860c039fe60a69e6549521f30e83b4ec6b22a212
luanabaratta <luanabaratta.technology@gmail.com>
2023-10-18 01:36:09 -0300

    adding migrations

--- BlazorEcommerce/BlazorEcommerce/Server/Controllers/ProductController.cs
+++ BlazorEcommerce/BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -7,38 +7,17 @@ namespace BlazorEcommerce.Server.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        private static List<Product> Products = new List<Product>
-        {
-            new Product
-            {
-                Id = 1,
-                Name = "Laranja",
-                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mi est, bibendum non consequat in, feugiat sit amet nisi. Nam mattis interdum sapien a egestas.",
-                ImageUrl = "https://images.unsplash.com/photo-1611080626919-7cf5a9dbab5b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80",
-                Price = 1.00m
-            },
-             new Product
-            {
-                Id = 2,
-                Name = "Cebola",
-                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mi est, bibendum non consequat in, feugiat sit amet nisi. Nam mattis interdum sapien a egestas.",
-                ImageUrl = "https://images.unsplash.com/photo-1618512496248-a07fe83aa8cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
-                Price = 0.99m
-            },
-             new Product
-            {
-                Id = 3,
-                Name = "Alface",
-                Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mi est, bibendum non consequat in, feugiat sit amet nisi. Nam mattis interdum sapien a egestas.",
-                ImageUrl = "https://plus.unsplash.com/premium_photo-1675011288480-abee1adc561d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
-                Price = 1.99m
-            },
-        };
+        private readonly DataContext _context;
+
+        public ProductController(DataContext context) {
+            _context = context;
+        }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProduct()
         {
-            return Ok(Products);
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
         }
     }
 }
---
+++ BlazorEcommerce/BlazorEcommerce/Server/Migrations/20231018033938_ProductSeeding.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BlazorEcommerce.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mi est, bibendum non consequat in, feugiat sit amet nisi. Nam mattis interdum sapien a egestas.", "https://images.unsplash.com/photo-1611080626919-7cf5a9dbab5b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80", "Laranja", 1.00m },
+                    { 2, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mi est, bibendum non consequat in, feugiat sit amet nisi. Nam mattis interdum sapien a egestas.", "https://images.unsplash.com/photo-1618512496248-a07fe83aa8cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80", "Cebola", 0.99m },
+                    { 3, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mi est, bibendum non consequat in, feugiat sit amet nisi. Nam mattis interdum sapien a egestas.", "https://plus.unsplash.com/premium_photo-1675011288480-abee1adc561d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80", "Alface", 1.99m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
