704babc8ec32a51b4ebc79c4ddeb60cac40a2fc9
Luana B. dos Santos <68722599+luanabaratta@users.noreply.github.com>
2023-10-25 14:17:05 -0300

    Add files via upload

--- BlazorEcommerce/BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/BlazorEcommerce/Server/Program.cs
@@ -1,13 +1,15 @@
+global using BlazorEcommerce.Server.Data;
+global using BlazorEcommerce.Server.Services.ProductService;
 global using BlazorEcommerce.Shared;
 global using Microsoft.EntityFrameworkCore;
-global using BlazorEcommerce.Server.Data;
-using Microsoft.AspNetCore.ResponseCompression;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<DataContext>(options => {
+builder.Services.AddDbContext<DataContext>(options =>
+{
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
@@ -16,6 +18,8 @@ builder.Services.AddRazorPages();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
