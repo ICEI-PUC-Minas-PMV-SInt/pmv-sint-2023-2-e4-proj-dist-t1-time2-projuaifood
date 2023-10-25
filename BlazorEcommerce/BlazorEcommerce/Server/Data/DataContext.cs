77df8f972be44d4b2f4ad1932aa0cbc6b1cde9a8
Luana B. dos Santos <68722599+luanabaratta@users.noreply.github.com>
2023-10-25 14:23:42 -0300

    Add files via upload

--- BlazorEcommerce/BlazorEcommerce/Server/Data/DataContext.cs
+++ BlazorEcommerce/BlazorEcommerce/Server/Data/DataContext.cs
@@ -2,7 +2,7 @@
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
 
         }
@@ -16,7 +16,7 @@
                         Name = "Laranja",
                         Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mi est, bibendum non consequat in, feugiat sit amet nisi. Nam mattis interdum sapien a egestas.",
                         ImageUrl = "https://images.unsplash.com/photo-1611080626919-7cf5a9dbab5b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80",
-                        Price = 1.00m
+                        Price = 1.99m
                     },
                      new Product
                      {
@@ -24,7 +24,7 @@
                          Name = "Cebola",
                          Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mi est, bibendum non consequat in, feugiat sit amet nisi. Nam mattis interdum sapien a egestas.",
                          ImageUrl = "https://images.unsplash.com/photo-1618512496248-a07fe83aa8cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
-                         Price = 0.99m
+                         Price = 2.99m
                      },
                      new Product
                      {
@@ -32,7 +32,7 @@
                          Name = "Alface",
                          Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mi est, bibendum non consequat in, feugiat sit amet nisi. Nam mattis interdum sapien a egestas.",
                          ImageUrl = "https://plus.unsplash.com/premium_photo-1675011288480-abee1adc561d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
-                         Price = 1.99m
+                         Price = 0.99m
                      }
                 );
         }
