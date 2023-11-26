3cd05aecf7b3ad1738a1ce6e980de47b50f3ad9f
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2023-11-26 00:49:34 -0300

    project is working

---
+++ BlazorEcommerce/BlazorEcommerce/Server/Migrations/20231109205731_ProductSeeding.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorEcommerce.Server.Migrations
+{
+    public partial class ProductSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[] { 1, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tristique in lacus in eleifend. Aenean vitae hendrerit metus. Donec vestibulum dui sed est fermentum efficitur.", "https://images.unsplash.com/photo-1550258987-190a2d41a8ba?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "Abacaxi", 3.99m });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[] { 2, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tristique in lacus in eleifend. Aenean vitae hendrerit metus. Donec vestibulum dui sed est fermentum efficitur.", "https://plus.unsplash.com/premium_photo-1671013032586-3e9a5c49ce3c?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "Laranja", 1.99m });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[] { 3, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tristique in lacus in eleifend. Aenean vitae hendrerit metus. Donec vestibulum dui sed est fermentum efficitur.", "https://plus.unsplash.com/premium_photo-1675715402461-9ac75a5b400e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "Abacate", 2.99m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
