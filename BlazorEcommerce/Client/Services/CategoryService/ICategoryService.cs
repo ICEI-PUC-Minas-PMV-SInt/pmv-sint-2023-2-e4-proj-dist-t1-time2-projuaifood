708b50049bb3fb55e58bb1b973417f0bbf5f0993
Luana B. dos Santos <luanabaratta@Luanas-MacBook-Air.local>
2023-11-26 00:48:01 -0300

    fix: Program.cs is building

--- BlazorEcommerce/Client/Services/CategoryService/ICategoryService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlazorEcommerce.Client.Services.CategoryService
-{
-    public interface ICategoryService
-    {
-        event Action OnChange;
-        List<Category> Categories { get; set; }
-        Task GetCategories();
-    }
-}
--- docs/diagrama-de-conteiner.md
+++
@@ -1,6 +0,0 @@
-# Diagrama de Contêiner
-
-![image](https://github.com/ICEI-PUC-Minas-PMV-SInt/pmv-sint-2023-2-e4-proj-dist-t1-time2-projuaifood/assets/101745127/63d9d437-b238-468c-b132-d06778bf0d5e)
-
-
-
